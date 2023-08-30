03b14d08704c7675c635f97e672eaa819d2dbad9
dprogrammer <kpidibadavid1@gmail.comm>
2023-08-30 16:35:19 +0000

    update with jdk 17

--- webview/src/App.java
+++ webview/src/App.java
@@ -1,24 +1,37 @@
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent arg0) {
+                System.out.println("Hello World!");
+            }
 
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-            Scene scene = new Scene(root);
-            scene.getStylesheets().add(getClass().getResource("main.css").toExternalForm());
-            primaryStage.setTitle("Google Preview");
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        });
 
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        /*
+         * Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+         * Scene scene = new Scene(root);
+         */
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
