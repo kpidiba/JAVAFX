051bf527ec161f6ccc6f3301148b9526900df02b
kbrightcoder <kpidibadavid1@gmail.com>
2024-05-04 18:51:14 +0000

    add new item

--- UI/settings/src/App.java
+++ UI/settings/src/App.java
@@ -1,14 +1,31 @@
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class App extends Application {
+    private double xOffSet = 0;
+    private double yOffSet = 0;
+
     @Override
     public void start(Stage arg0) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("fxml/main.fxml"));
+        arg0.initStyle(StageStyle.TRANSPARENT);
+        root.setOnMousePressed(event -> {
+            xOffSet = event.getSceneX();
+            yOffSet = event.getSceneY();
+        });
+        root.setOnMouseDragged(event -> {
+            arg0.setX(event.getScreenX() - xOffSet);
+            arg0.setY(event.getScreenY() - yOffSet);
+        });
         Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
         arg0.setScene(scene);
         arg0.show();
     }
