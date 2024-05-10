238c29c635f8cafc2fa1d4b848f30e1ca7ee41df
kbrightcoder <kpidibadavid1@gmail.com>
2024-05-10 11:02:55 +0000

    start menu bar project

--- UI/menu_bar_effect/src/App.java
+++ UI/menu_bar_effect/src/App.java
@@ -16,7 +16,7 @@ public class App extends Application {
     public void start(Stage arg0) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("fxml/app.fxml"));
         Scene scene = new Scene(root);
-        arg0.setTitle("SETTINGS");
+        arg0.setTitle("MENU BAR EFFECT");
         arg0.setScene(scene);
         arg0.show();
     }
