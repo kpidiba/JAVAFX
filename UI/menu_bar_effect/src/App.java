026775f3789b7eee323de451a90a12761866d3ac
kbrightcoder <kpidibadavid1@gmail.com>
2024-05-10 10:31:10 +0000

    launch new project

---
+++ UI/menu_bar_effect/src/App.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/app.fxml"));
+        Scene scene = new Scene(root);
+        arg0.setTitle("SETTINGS");
+        arg0.setScene(scene);
+        arg0.show();
+    }
+
+}
