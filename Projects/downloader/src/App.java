0b1bf88a095cf45203805d17f8c1fed8bda4a5ce
kbrightcoder <kpidibadavid1@gmail.com>
2024-05-13 15:54:08 +0000

    initialisation du projet

--- Projects/downloader/src/App.java
+++ Projects/downloader/src/App.java
@@ -1,5 +1,21 @@
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        launch(args);
     }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        Parent root= FXMLLoader.load(getClass().getResource("./fxml/app.fxml"));
+        Scene scene = new Scene(root);
+        arg0.setScene(scene);
+        arg0.show();
+    }
+
+
 }
