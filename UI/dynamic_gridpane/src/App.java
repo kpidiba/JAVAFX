7e05910550ac5a6793dda5f5befe4e99ba36388d
kbrightcoder <kpidibadavid1@gmail.com>
2024-06-05 18:05:55 +0000

    add card to grid pane

--- UI/dynamic_gridpane/src/App.java
+++ UI/dynamic_gridpane/src/App.java
@@ -12,11 +12,12 @@ public class App extends Application {
     @Override
     public void start(Stage arg0) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("./fxml/main/app.fxml"));
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, 900, 600);
         arg0.setTitle("DYNAMIC GRID PANE");
+        arg0.setScene(scene);
         arg0.setMinWidth(root.minWidth(-1));
         arg0.setMinHeight(root.minHeight(-1));
-        arg0.setScene(scene);
+        arg0.setResizable(false);
         arg0.show();
     }
 }
